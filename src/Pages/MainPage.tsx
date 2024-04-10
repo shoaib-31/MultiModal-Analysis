fe42213be047bebb9aabec11958ca64e197ba650
Shoaib Akhtar <shoaibakmasood@gmail.com>
2024-04-11 05:01:29 +0530

    started

---
+++ src/Pages/MainPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SimpleMap from "../Components/Map";
+
+const MainPage = () => {
+  return (
+    <div className=" flex flex-col items-center justify-center w-screen h-screen">
+      <SimpleMap />
+    </div>
+  );
+};
+
+export default MainPage;
